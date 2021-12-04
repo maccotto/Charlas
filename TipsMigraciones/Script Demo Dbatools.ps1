5d5be04cd617c51e74aca6c52b5260c1feb39e73
Maximiliano Accotto <maxi.accotto@gmail.com>
2021-12-04 08:44:15 -0300

    Add files via upload

---
+++ TipsMigraciones/Script Demo Dbatools.ps1
@@ -0,0 +1,16 @@
+﻿import-module dbatools
+
+$CRED = Get-Credential sa
+
+
+Copy-DbaDatabase -Source "localhost,14033" -Destination "localhost,15033" -Database Adventureworks2017 -BackupRestore -SharedPath /var/opt/mssql/share -SourceSqlCredential $CRED -DestinationSqlCredential $CRED -SetSourceReadOnly -Force 
+
+Copy-DbaLogin -Source "localhost,14033" -Destination "localhost,15033" -Force -KillActiveConnection -SourceSqlCredential $CRED -DestinationSqlCredential $CRED
+
+
+Repair-DbaDbOrphanUser -SqlInstance "localhost,15033" -SqlCredential $CRED
+
+Set-DbaDbCompatibility -SqlInstance "localhost,15033" -SqlCredential $cred 
+
+
+
